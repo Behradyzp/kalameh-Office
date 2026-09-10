9813539bb34bee6a26db719fdfd1881a6c7f678a
Behradyzp <Behradyzp@gmail.com>
2026-09-10 12:14:55 +0330

    Supabase migration: update cpanel-src/main.tsx

--- cpanel-src/main.tsx
+++ cpanel-src/main.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import Home from "../app/page";
 import "../app/globals.css";
+import { installSupabaseApiBridge } from "../lib/supabase-api-bridge";
+
+installSupabaseApiBridge();
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
